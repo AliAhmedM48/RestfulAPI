bd33c3b47b6490d4908fb7082d80af6088fb4235
Abdulrhaman-Mohamed <abdorrr2015@gmail.com>
2024-02-21 01:38:32 +0200

    add CORS

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
+
 const userRoute = require("./routes/user.route");
 const authRoute = require("./routes/auth.router");
 
@@ -16,7 +18,7 @@ const PORT =  process.env.PORT||5000;
 
 
 
-
+app.use(cors());
 app.use(express.json());
 app.get("/users", async(req,res)=>{
   try
