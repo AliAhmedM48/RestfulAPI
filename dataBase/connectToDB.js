88a2f6107e29baca276c16aeba0be7dfc30c7e28
Ali Ahmed <63220520+AliAhmedM48@users.noreply.github.com>
2024-02-20 22:05:02 +0200

    Update connectToDB.js

--- dataBase/connectToDB.js
+++ dataBase/connectToDB.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 let URL_DATABASE = process.env.URL_DATABASE;
+const DB_NAME_MONGO_ATLAS = process.env.MONGOatlas_DB_NAME;
 
 let connect = () => {
   const connectDB = mongoose
